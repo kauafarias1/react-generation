bc94775db0b63f8b4f33648492b7114c3cb116bc
kaua <kauagabrielfarias18@gmail.com>
2026-01-15 14:46:47 -0300

    Style login & signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Home />
-    <Footer />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/cadastro"	element={<Cadastro />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
     </>
   );
 }
